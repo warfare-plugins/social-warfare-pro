81dc05fa530627899e54e598a1de3f42f6c5ddc3
Nicholas Cardot <nick@sitesketch101.com>
2017-03-09 14:46:27 -0600

    Added the output html for the pinterest image for browser extensions
    The image is now properly output either hidden, at the top of the post,
    or at the bottom of the post.

--- functions/utility.php
+++ functions/utility.php
@@ -24,3 +24,98 @@ function swp_get_site_url() {
 
 	return $domain;
 }
+
+/**
+ * A function to queue up the function that will add the Pinterest image for browser extensions
+ *
+ * @since 2.2.3 | Created | 09 MAR 2017
+ * @param none
+ * @return none
+ */
+function swp_pre_insert_pinterest_image() {
+
+	// Only hook into the_content filter if we is_singular() is true or they don't use excerpts
+    if( true === is_singular() ):
+        add_filter( 'the_content','swp_insert_pinterest_image', 10 );
+    endif;
+
+}
+
+// Hook into the template_redirect so that is_singular() conditionals will be ready
+add_action('template_redirect', 'swp_pre_insert_pinterest_image');
+
+/**
+ * A function to insert the Pinterest image for browser extensions
+ *
+ * @since  2.2.4 | Created | 09 MAR 2017
+ * @access public
+ * @param  string $content The post content to filter
+ * @return string $content The filtered content
+ */
+function swp_insert_pinterest_image( $content ) {
+
+	// Fetch the user's settings
+	global $swp_user_options, $post;
+	$post_id = $post->ID;
+
+	// First make sure this feature is activated
+	if( isset( $swp_user_options['advanced_pinterest_image'] ) && true === $swp_user_options['advanced_pinterest_image'] ):
+
+		// Collect the user's custom defined Pinterest specific Image
+		$pinterest_image_url = get_post_meta( $post_id, 'swp_pinterest_image_url' , true );
+
+		// Exit if the user doesn't have a custom defined image
+		if( false !== $pinterest_image_url && !empty($pinterest_image_url) ):
+
+			// Fetch the user's custom Pinterest description
+			$pinterest_description = get_post_meta( $post_id , 'nc_pinterestDescription' , true );
+
+			// Collect the user's Pinterest username
+			if ( !empty( $swp_user_options['pinterestID'] ) ) :
+				$pinterest_username = ' via @' . str_replace( '@' , '' , $swp_user_options['pinterestID'] );
+			else :
+				$pinterest_username = '';
+			endif;
+
+			// If there is no custom description, use the post Title
+			if( false === $pinterest_description || empty($pinterest_image_url) ):
+				urlencode( html_entity_decode( get_the_title() . $pinterest_username, ENT_COMPAT, 'UTF-8' ) );
+			endif;
+
+			// Fetch the Permalink
+			$permalink = get_the_permalink();
+
+			// Check if this image is hidden and add display:none to it.
+			if( isset( $swp_user_options['advanced_pinterest_image_location'] ) && 'hidden' === $swp_user_options['advanced_pinterest_image_location'] ) :
+
+				// Compile the image
+				$image_html = '<img style="display:none;" src="'.$pinterest_image_url.'" data-pin-url="'.$permalink.'" data-pin-media="'.$pinterest_image_url.'" data-pin-description="'.$pinterest_description.'" />';
+
+				// Add the hidden image to the content string
+				$content .= $image_html;
+
+			// Check if the this image is not hidden and do not add display:none to it.
+			elseif( isset( $swp_user_options['advanced_pinterest_image_location'] ) && 'hidden' !== $swp_user_options['advanced_pinterest_image_location'] ) :
+
+				// Compile the image
+				$image_html = '<img class="no_pin" src="'.$pinterest_image_url.'" alt="'.$pinterest_description.'" data-pin-url="'.$permalink.'" data-pin-media="'.$pinterest_image_url.'" data-pin-description="'.$pinterest_description.'" />';
+
+				// Add the visible image to the top of the content
+				if('top' === $swp_user_options['advanced_pinterest_image_location']):
+					$content = $image_html . $content;
+				endif;
+
+				// Add the visible image to the bottom of the content
+				if('bottom' === $swp_user_options['advanced_pinterest_image_location']):
+					$content .= $image_html;
+				endif;
+
+			endif;
+
+		endif;
+
+	endif;
+
+	return $content;
+
+}
