b66741d8b37e5e8bdbb652a02004f23b39bf9479
Nicholas Cardot <nick@sitesketch101.com>
2017-05-04 08:33:47 -0500

    Refactored and updated docblocking in whatsapp.php

--- functions/social-networks/whatsapp.php
+++ functions/social-networks/whatsapp.php
@@ -1,17 +1,34 @@
 <?php
 
 /**
- * #1: Add the On / Off Switch	and Sortable Option
+ * Functions to add a WhatsApp share button to the available buttons
+ *
+ * @package   SocialWarfare\Functions
+ * @copyright Copyright (c) 2017, Warfare Plugins, LLC
+ * @license   GPL-3.0+
+ * @since     1.0.0 | CREATED | Unknown
+ * @since     2.2.4 | UPDATED | 4 MAY 2017 | Refactored functions & updated docblocking
  */
-add_filter( 'swp_button_options', 'swp_whatsapp_options_function',20 );
+
+defined( 'WPINC' ) || die;
+
+/**
+ * #1: Add the On/Off Switch and Sortable Option
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array $options The array of available plugin options
+ * @return array $options The modified array of available plugin options
+ */
+add_filter( 'swp_button_options' , 'swp_whatsapp_options_function' , 20 );
 function swp_whatsapp_options_function( $options ) {
 
 	// Create the new option in a variable to be inserted
 	$options['content']['whatsapp'] = array(
-	'type' => 'checkbox',
-	'content' => 'WhatsApp',
-	'default' => false,
-	'premium' => true,
+		'type' => 'checkbox',
+		'content' => 'WhatsApp',
+		'default' => false,
+		'premium' => true,
 	);
 
 	return $options;
@@ -19,75 +36,86 @@ function swp_whatsapp_options_function( $options ) {
 };
 
 /**
- * #2: Add it to global network array
+ * #2: Add it to the global network array
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array $networks The array of available plugin social networks
+ * @return array $networks The modified array of available plugin social networks
  */
-// Queue up your filter to be ran on the swp_options hook.
 add_filter( 'swp_add_networks', 'swp_whatsapp_network' );
-
-// Create the function that will filter the options
 function swp_whatsapp_network( $networks ) {
-
-	// Add your network to the existing network array
 	$networks[] = 'whatsapp';
-
-	// Be sure to return the modified options array or the world will explode
 	return $networks;
 };
 
 /**
- *  #3: Generate the API Share Count Request URL
+ * #3: Generate the API Share Count Request URL
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  string $url The permalink of the page or post for which to fetch share counts
+ * @return int 0 WhatsApp doesn't offer share counts
  */
 function swp_whatsapp_request_link( $url ) {
-	$request_url = 'https://api.facebook.com/restserver.php?method=links.getStats&format=json&urls=' . $url;
 	return 0;
 }
 
 /**
- * #4: Parse the Response to get the share count
+ * #4: Parse the response to get the share count
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  string $response The raw response returned from the API request
+ * @return int 0 WhatsApp doesn't offer share counts
  */
 function swp_format_whatsapp_response( $response ) {
-	// $response = json_decode($response, true);
-	// return isset($response[0]['total_count'])?intval($response[0]['total_count']):0;
 	return 0;
 }
+
 /**
- * #5: Create the Button HTML
+ * #5: Create the HTML to display the share button
+ *
+ * @since  1.0.0
+ * @access public
+ * @param  array $array The array of information used to create and display each social panel of buttons
+ * @return array $array The modified array which will now contain the html for this button
  */
-add_filter( 'swp_network_buttons', 'swp_whatsapp_button_html',10 );
+add_filter( 'swp_network_buttons' , 'swp_whatsapp_button_html' , 10 );
 function swp_whatsapp_button_html( $array ) {
 
 	// If we've already generated this button, just use our existing html
 	if ( isset( $_GLOBALS['sw']['buttons'][ $array['postID'] ]['whatsapp'] ) ) :
 		$array['resource']['whatsapp'] = $_GLOBALS['sw']['buttons'][ $array['postID'] ]['whatsapp'];
 
-		// If not, let's check if WhatsApp is activated and create the button HTML
-		elseif ( (isset( $array['options']['newOrderOfIcons']['whatsapp'] ) && ! isset( $array['buttons'] ))
-		|| (isset( $array['buttons'] ) && isset( $array['buttons']['whatsapp'] ))  ) :
+	// If not, let's check if WhatsApp is activated and create the button HTML
+	elseif ( (isset( $array['options']['newOrderOfIcons']['whatsapp'] ) && ! isset( $array['buttons'] ))
+	|| (isset( $array['buttons'] ) && isset( $array['buttons']['whatsapp'] ))  ) :
 
-			$array['totes'] += intval( $array['shares']['whatsapp'] );
-			++$array['count'];
+		$array['totes'] += intval( $array['shares']['whatsapp'] );
+		++$array['count'];
 
-			$array['resource']['whatsapp'] = '<div class="nc_tweetContainer swp_whatsapp" data-id="' . $array['count'] . '" data-network="whatsapp">';
-			$link = urlencode( urldecode( swp_process_url( $array['url'] , 'whatsapp' , $array['postID'] ) ) );
-			$array['resource']['whatsapp'] .= '<a rel="nofollow" target="_blank" onclick="window.location = this.href;" href="whatsapp://send?text=' . $link . '" data-link="whatsapp://send?text=' . $link . '" class="nc_tweet noPop" data-action="share/whatsapp/share">';
-			if ( $array['options']['totesEach'] && $array['shares']['totes'] >= $array['options']['minTotes'] && $array['shares']['whatsapp'] > 0 ) :
-				$array['resource']['whatsapp'] .= '<span class="iconFiller">';
-				$array['resource']['whatsapp'] .= '<span class="spaceManWilly">';
-				$array['resource']['whatsapp'] .= '<i class="sw sw-whatsapp"></i>';
-				$array['resource']['whatsapp'] .= '<span class="swp_share"> ' . __( 'WhatsApp','social-warfare' ) . '</span>';
-				$array['resource']['whatsapp'] .= '</span></span>';
-				$array['resource']['whatsapp'] .= '<span class="swp_count">' . swp_kilomega( $array['shares']['whatsapp'] ) . '</span>';
-			else :
-				$array['resource']['whatsapp'] .= '<span class="swp_count swp_hide"><span class="iconFiller"><span class="spaceManWilly"><i class="sw sw-whatsapp"></i><span class="swp_share"> ' . __( 'WhatsApp','social-warfare' ) . '</span></span></span></span>';
-			endif;
-			$array['resource']['whatsapp'] .= '</a>';
-			$array['resource']['whatsapp'] .= '</div>';
-
-			// Store these buttons so that we don't have to generate them for each set
-			$_GLOBALS['sw']['buttons'][ $array['postID'] ]['whatsapp'] = $array['resource']['whatsapp'];
-
+		$array['resource']['whatsapp'] = '<div class="nc_tweetContainer swp_whatsapp" data-id="' . $array['count'] . '" data-network="whatsapp">';
+		$link = urlencode( urldecode( swp_process_url( $array['url'] , 'whatsapp' , $array['postID'] ) ) );
+		$array['resource']['whatsapp'] .= '<a rel="nofollow" target="_blank" onclick="window.location = this.href;" href="whatsapp://send?text=' . $link . '" data-link="whatsapp://send?text=' . $link . '" class="nc_tweet noPop" data-action="share/whatsapp/share">';
+		if ( $array['options']['totesEach'] && $array['shares']['totes'] >= $array['options']['minTotes'] && $array['shares']['whatsapp'] > 0 ) :
+			$array['resource']['whatsapp'] .= '<span class="iconFiller">';
+			$array['resource']['whatsapp'] .= '<span class="spaceManWilly">';
+			$array['resource']['whatsapp'] .= '<i class="sw sw-whatsapp"></i>';
+			$array['resource']['whatsapp'] .= '<span class="swp_share"> ' . __( 'WhatsApp','social-warfare' ) . '</span>';
+			$array['resource']['whatsapp'] .= '</span></span>';
+			$array['resource']['whatsapp'] .= '<span class="swp_count">' . swp_kilomega( $array['shares']['whatsapp'] ) . '</span>';
+		else :
+			$array['resource']['whatsapp'] .= '<span class="swp_count swp_hide"><span class="iconFiller"><span class="spaceManWilly"><i class="sw sw-whatsapp"></i><span class="swp_share"> ' . __( 'WhatsApp','social-warfare' ) . '</span></span></span></span>';
 		endif;
+		$array['resource']['whatsapp'] .= '</a>';
+		$array['resource']['whatsapp'] .= '</div>';
 
-		return $array;
+		// Store these buttons so that we don't have to generate them for each set
+		$_GLOBALS['sw']['buttons'][ $array['postID'] ]['whatsapp'] = $array['resource']['whatsapp'];
+
+	endif;
+
+	return $array;
 
 };
