22fb10a6b1152473de00023a4c0f2a827e13a0f9
Nicholas Cardot <nick@sitesketch101.com>
2017-09-21 13:59:34 -0500

    Removed the force new shares option

--- functions/admin/options-array.php
+++ functions/admin/options-array.php
@@ -1146,13 +1146,6 @@ function swp_pro_options_advanced($swp_options) {
 		'force_new_shares_description' => array(
 			'type'		=> 'paragraph',
 			'content'	=> __( 'If the API share count request returns a lower number than previously recorded, we ignore the new number and retain the original higher number from the previous request. Activating this will force the new share number to be accepted even if it is a lower number than previously recorded.' , 'social-warfare' )
-		),
-		'force_new_shares'		=> array(
-			'type'				=> 'checkbox',
-			'size'				=> 'two-thirds',
-			'content'				=> __( 'Force New Shares?' , 'social-warfare' ),
-			'default'			=> false,
-			'premium'			=> false,
 		)
 	);
 
