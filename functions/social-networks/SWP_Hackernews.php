731510c5bad5e5a9dd60a436e9b8a233628a70dc
ckmahoney <cortland.mahoney@gmail.com>
2018-04-30 21:01:04 -0400

    Added the title as query parameter.

--- functions/social-networks/SWP_Hackernews.php
+++ functions/social-networks/SWP_Hackernews.php
@@ -70,4 +70,25 @@ class SWP_HackerNews extends SWP_Social_Network {
         $response = json_decode( $response, true );
         return $response['nbHits'];
     }
+
+    /**
+     * Generate the share link
+     *
+     * This is the link that is being clicked on which will open up the share
+     * dialogue. Thie method is only used for networks that use this exact same pattern.
+     * For anything that accepts more than just the post permalink as a URL parameter,
+     * those networks will have to overwrite this method with their own custom method
+     * in their respective child classes.
+     *
+     * @since  3.0.0 | 08 APR 2018 | Created
+     * @param  array $array The array of information passed in from the buttons panel.
+     * @return string The generated link
+     * @access public
+     *
+     */
+    public function generate_share_link( $post_data ) {
+        $title = urldecode( $post_data['post_title'] );
+        $share_link = $this->base_share_url . $this->get_shareable_permalink( $post_data ) . '&t=' . $title;
+        return $share_link;
+    }
 }
