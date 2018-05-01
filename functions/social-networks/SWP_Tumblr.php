0d0ebb39c593e859189ca68f438eb109ed586012
ckmahoney <cortland.mahoney@gmail.com>
2018-04-30 21:00:52 -0400

    Updated to use new share API.

--- functions/social-networks/SWP_Tumblr.php
+++ functions/social-networks/SWP_Tumblr.php
@@ -39,7 +39,7 @@ class SWP_Tumblr extends SWP_Social_Network {
 		$this->key            = 'tumblr';
 		$this->default        = false;
         $this->premium        = 'pro';
-		$this->base_share_url = 'http://www.tumblr.com/share/link?url=';
+		$this->base_share_url = 'https://www.tumblr.com/widgets/share/tool?';
 
 		$this->init_social_network();
 	}
@@ -72,4 +72,27 @@ class SWP_Tumblr extends SWP_Social_Network {
         $response = json_decode( $response, true );
     	return isset( $response['response']['note_count'] ) ? intval( $response['response']['note_count'] ) : 0;
 	}
+
+    /**
+     * Generate the share link
+     *
+     * This is the link that is being clicked on which will open up the share
+     * dialogue. Thie method is only used for networks that use this exact same pattern.
+     * For anything that accepts more than just the post permalink as a URL parameter,
+     * those networks will have to overwrite this method with their own custom method
+     * in their respective child classes.
+     *
+     * @since  3.0.0 | 08 APR 2018 | Created
+     * @param  array $array The array of information passed in from the buttons panel.
+     * @return string The generated link
+     * @access public
+     *
+     */
+    public function generate_share_link( $post_data ) {
+        $parameters = 'posttype=link';
+        $parameters .= '&canonicalUrl=' . $this->get_shareable_permalink( $post_data);
+        $parameters .= '&title=' . urlencode( $post_data['post_title'] );
+        $share_link = $this->base_share_url . $parameters;
+        return $share_link;
+    }
 }
