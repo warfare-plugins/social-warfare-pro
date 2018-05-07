400eaaeab06077a21359d1c68c949da9116192ce
ckmahoney <cortland.mahoney@gmail.com>
2018-05-06 22:15:09 -0400

    Added footer hook, removed related old file.

--- social-warfare-pro.php
+++ social-warfare-pro.php
@@ -27,10 +27,55 @@ function initialize_social_warfare_pro() {
 	if( defined('SWP_VERSION') && SWP_VERSION == SWPP_VERSION ):
 		require_once SWPP_PLUGIN_DIR . '/functions/Social_Warfare_Pro.php';
 		new Social_Warfare_Pro();
+
+        // Queue up out footer hook function
+        add_filter( 'swp_footer_scripts', 'swp_pinit_controls_output');
+
     else:
         add_action( 'admin_notices', 'mismatch_notification' );
 	endif;
 }
+
+
+
+/**
+ * A function to output the Pin Button option controls
+ *
+ * @since  2.1.4
+ * @access public
+ * @param  array $info An array of footer script information.
+ * @return array $info A modified array of footer script information.
+ */
+function swp_pinit_controls_output($info){
+	global $swp_user_options;
+
+	$pin_vars = array(
+		'enabled' => false,
+	);
+
+		if ( $swp_user_options['pinit_toggle'] ) {
+			$pin_vars['enabled']   = true;
+			$pin_vars['hLocation'] = $swp_user_options['pinit_location_horizontal'];
+			$pin_vars['vLocation'] = $swp_user_options['pinit_location_vertical'];
+			$pin_vars['minWidth']  = str_replace( 'px', '', $swp_user_options['pinit_min_width'] );
+			$pin_vars['minHeight'] = str_replace( 'px', '', $swp_user_options['pinit_min_height'] );
+
+			// Set the image source
+			if(isset($swp_user_options['pinit_image_source']) && 'custom' == $swp_user_options['pinit_image_source'] && get_post_meta( get_the_ID() , 'swp_pinterest_image_url' , true ) ):
+				$pin_vars['image_source'] = get_post_meta( get_the_ID() , 'swp_pinterest_image_url' , true );
+			endif;
+
+			// Set the description Source
+			if(isset($swp_user_options['pinit_image_description']) && 'custom' == $swp_user_options['pinit_image_description'] && get_post_meta( get_the_ID() , 'nc_pinterest_description' , true ) ):
+				$pin_vars['image_description'] = get_post_meta( get_the_ID() , 'nc_pinterest_description' , true );
+			endif;
+
+		}
+	$info['footer_output'] .= ' swpPinIt='.json_encode($pin_vars).';';
+	return $info;
+}
+
+
 
 /**
  * Notify users that the versions of Social Warfare and Social Warfare Pro are mismatched.
