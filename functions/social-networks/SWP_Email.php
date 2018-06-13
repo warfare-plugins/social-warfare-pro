25577ad7a5828a5d9eb73092ae53ea4005bc3e5f
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-06-13 16:18:39 -0400

    Added the nofollow and noopener attributes.

--- functions/social-networks/SWP_Email.php
+++ functions/social-networks/SWP_Email.php
@@ -108,7 +108,7 @@ class SWP_Email extends SWP_Social_Network {
 
 		// Build the wrapper.
 		$html = '<div class="nc_tweetContainer swp_'.$this->key.'" data-network="'.$this->key.'">';
-			$html .= '<a rel="noreferrer" href="' . $share_link . '" class="nc_tweet">';
+			$html .= '<a rel="nofollow noreferrer noopener" href="' . $share_link . '" class="nc_tweet">';
 				// Put the button inside.
 				$html .= $icon;
 			$html.= '</a>';
