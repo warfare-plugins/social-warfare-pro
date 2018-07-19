1e74da327f50e16bd15bd0c14e26fcc56c19e4f5
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-07-19 16:04:48 -0400

    Added update_notice filter and funciton. #575

--- social-warfare-pro.php
+++ social-warfare-pro.php
@@ -32,6 +32,9 @@ function initialize_social_warfare_pro() {
     //* If the versions are not up to date, we need to display a message
     //* saying somthing like "To reap all the features of Pro, update to the latest version."
 
+    if ( defined( 'SWP_VERSION' ) && SWP_VERSION != SWPP_VERSION ) {
+        add_filter( 'swp_admin_notices', 'update_notification' );
+    }
 
 	// if( defined('SWP_VERSION') && SWP_VERSION == SWPP_VERSION ):
         if ( file_exists( SWPP_PLUGIN_DIR . '/functions/Social_Warfare_Pro.php' ) ) :
@@ -50,10 +53,6 @@ function initialize_social_warfare_pro() {
     //     add_action( 'admin_notices', 'mismatch_notification' );
     //     return;
 	// endif;
-
-	if ( !class_exists( 'EDD_SL_Plugin_Updater' ) ) {
-        require_once SWPP_PLUGIN_DIR . '/functions/utilities/EDD_SL_Plugin_Updater.php';
-    }
 
     /**
      * Note regarding keys:
@@ -84,6 +83,10 @@ function initialize_social_warfare_pro() {
 
     //* An expired Pro key.
     // $expired_key = 'ab81a8227a5ee7a180ca2dbf89b5b935';
+
+    if ( !class_exists( 'EDD_SL_Plugin_Updater' ) && defined( 'SWP_PLUGIN_DIR' ) ) {
+        require_once( SWP_PLUGIN_DIR . '/functions/utilities/EDD_SL_Plugin_Updater.php' );
+    }
 
     $edd_updater = new EDD_SL_Plugin_Updater( EDD_SL_STORE_URL, __FILE__, array(
     	'version' 	=> SWPP_VERSION,		// current version number
@@ -154,4 +157,36 @@ function needs_core() {
  */
  function mismatch_notification() {
     echo '<div class="update-nag notice is-dismissable"><p><b>Important:</b> You are currently running Social Warfare v' . SWP_VERSION . ' and Social Warfare - Pro v' . SWPP_VERSION . '. In order to avoid conflicts, these two version need to match in order to activate all of the plugins features. Please update the appropriate plugin so that both Social Warfare and Social Warfare - Pro are on the same version. For more information about this, <a href="https://warfareplugins.com/support/updating-social-warfare-social-warfare-pro/">please read this</a></p></div>';
+}
+
+/**
+ * Notify users that the versions of Social Warfare and Social Warfare Pro are mismatched.
+ *
+ *
+ * @since  2.2.0
+ * @param  none
+ * @return void
+ *
+ */
+ function update_notification( $notices = array() ) {
+     if (is_string( $notices ) ) {
+         $notices = array();
+     }
+
+     $notices[] = array(
+         'key'   => 'update_notice_pro_' . SWPP_VERSION, // database key unique to this version.
+         'message'   => 'Looks like your copy of Social Warfare - Pro isn\t up to date with Core. While you can still use both of these plugins, we highly recommend you keep both Core and Pro up-to-date for the best of what we have to offer.',
+         'ctas'  => array(
+             array(
+                 'action'    => 'Remind me in a week.',
+                 'timeframe' => 7 // permadismiss for this version.
+             ),
+             array(
+                 'action'    => 'Thanks for letting me know.',
+                 'timeframe' => 0 // permadismiss for this version.
+             )
+         )
+     );
+
+     return $notices;
 }
