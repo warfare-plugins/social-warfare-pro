5a93ea080648214f743ed104522d7da6ecd914c5
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-07-19 15:17:27 -0400

    Uncommented required code.

--- social-warfare-pro.php
+++ social-warfare-pro.php
@@ -34,14 +34,14 @@ function initialize_social_warfare_pro() {
 
 
 	// if( defined('SWP_VERSION') && SWP_VERSION == SWPP_VERSION ):
-    //     if ( file_exists( SWPP_PLUGIN_DIR . '/functions/Social_Warfare_Pro.php' ) ) :
-    // 		require_once SWPP_PLUGIN_DIR . '/functions/Social_Warfare_Pro.php';
-    // 		new Social_Warfare_Pro();
-    //
-    //         // Queue up out footer hook function
-    //         add_filter( 'swp_footer_scripts', 'swp_pinit_controls_output');
-    //
-    //     endif;
+        if ( file_exists( SWPP_PLUGIN_DIR . '/functions/Social_Warfare_Pro.php' ) ) :
+    		require_once SWPP_PLUGIN_DIR . '/functions/Social_Warfare_Pro.php';
+    		new Social_Warfare_Pro();
+
+            // Queue up out footer hook function
+            add_filter( 'swp_footer_scripts', 'swp_pinit_controls_output');
+
+        endif;
     //
     // elseif ( !defined('SWP_VERSION')) :
     //     add_action( 'admin_notices', 'needs_core' );
