136b98a9dbe3a71b5d5a85dee9a193d63bb3d57a
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-08-21 11:33:01 -0400

    Moved filter to SWP_Pro_Pinterest; formatting.

--- social-warfare-pro.php
+++ social-warfare-pro.php
@@ -25,9 +25,9 @@ define( 'SWPP_SL_PRODUCT_ID', 189418 );  // Pro Utility Product id
 add_action('plugins_loaded' , 'initialize_social_warfare_pro' , 20 );
 
 function initialize_social_warfare_pro() {
+
     if ( !defined( 'SWP_VERSION' ) ) :
-        //* The original notice saying Core is required for Pro.
-        //* Note: This notice is not permadismissable. It is just a normal admin notice.
+        //* We can not do any Pro without Core. Let them know and bail.
         add_action( 'admin_notices', 'swp_needs_core' );
         return;
     endif;
@@ -37,13 +37,10 @@ function initialize_social_warfare_pro() {
     endif;
 
 
-	if( defined('SWP_VERSION') && SWP_VERSION == SWPP_VERSION ):
+	if( defined( 'SWP_VERSION' ) && SWP_VERSION == SWPP_VERSION ):
         if ( file_exists( SWPP_PLUGIN_DIR . '/lib/Social_Warfare_Pro.php' ) ) :
     		require_once SWPP_PLUGIN_DIR . '/lib/Social_Warfare_Pro.php';
     		new Social_Warfare_Pro();
-
-            // Queue up out footer hook function
-            add_filter( 'swp_footer_scripts', 'swp_pinit_controls_output');
         endif;
     else:
         //* Do not instantiate Pro. Instead make them update.
@@ -51,55 +48,17 @@ function initialize_social_warfare_pro() {
 	endif;
 
     if ( class_exists( 'Puc_v4_Factory') ) :
-
         $update_checker = Puc_v4_Factory::buildUpdateChecker(
         	'https://github.com/warfare-plugins/social-warfare-pro/',
         	__FILE__,
         	'social-warfare-pro'
         );
-
         $update_checker->getVcsApi()->enableReleaseAssets();
-
     endif;
+
 }
 
-/**
- * A function to output the Pin Button option controls
- *
- * @since  2.1.4
- * @access public
- * @param  array $info An array of footer script information.
- * @return array $info A modified array of footer script information.
- */
-function swp_pinit_controls_output($info){
-	global $swp_user_options;
 
-	$pin_vars = array(
-		'enabled' => false,
-	);
-
-	if ( $swp_user_options['pinit_toggle'] ) {
-		$pin_vars['enabled']   = true;
-		$pin_vars['hLocation'] = $swp_user_options['pinit_location_horizontal'];
-		$pin_vars['vLocation'] = $swp_user_options['pinit_location_vertical'];
-		$pin_vars['minWidth']  = str_replace( 'px', '', $swp_user_options['pinit_min_width'] );
-		$pin_vars['minHeight'] = str_replace( 'px', '', $swp_user_options['pinit_min_height'] );
-        $pin_vars['disableOnAnchors'] = $swp_user_options['pinit_hide_on_anchors'];
-
-		// Set the image source
-		if(isset($swp_user_options['pinit_image_source']) && 'custom' == $swp_user_options['pinit_image_source'] && get_post_meta( get_the_ID() , 'swp_pinterest_image_url' , true ) ):
-			$pin_vars['image_source'] = get_post_meta( get_the_ID() , 'swp_pinterest_image_url' , true );
-		endif;
-
-		// Set the description Source
-		if(isset($swp_user_options['pinit_image_description']) && 'custom' == $swp_user_options['pinit_image_description'] && get_post_meta( get_the_ID() , 'swp_pinterest_description' , true ) ):
-			$pin_vars['image_description'] = get_post_meta( get_the_ID() , 'swp_pinterest_description' , true );
-		endif;
-	}
-
-	$info['footer_output'] .= ' swpPinIt='.json_encode($pin_vars).';';
-	return $info;
-}
 
 
 if ( !function_exists( 'swp_needs_core' ) ) :
