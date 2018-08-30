fd94bea9138f04fff27a6b0cf414989080103fa3
Nicholas Cardot <nick@sitesketch101.com>
2018-08-30 13:44:37 -0500

    Fixed typo in version comparison.

--- social-warfare-pro.php
+++ social-warfare-pro.php
@@ -37,7 +37,7 @@ function initialize_social_warfare_pro() {
     endif;
 
 
-	if( defined( 'SWP_VERSION' ) && version_compare( SWP_VERSION , 2.9.0 ) >= 0 ):
+	if( defined( 'SWP_VERSION' ) && version_compare( SWP_VERSION , 3.2.90 ) >= 0 ):
         if ( file_exists( SWPP_PLUGIN_DIR . '/lib/Social_Warfare_Pro.php' ) ) :
     		require_once SWPP_PLUGIN_DIR . '/lib/Social_Warfare_Pro.php';
     		new Social_Warfare_Pro();
