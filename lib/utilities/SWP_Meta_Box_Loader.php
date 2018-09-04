436af272dd9365e1599fdcad000e7523553536d4
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-09-04 15:20:12 -0400

    Updated customTweetWrapper to custom_tweetWrapper. warfare-plugins/social-warfare#617

--- lib/utilities/SWP_Meta_Box_Loader.php
+++ lib/utilities/SWP_Meta_Box_Loader.php
@@ -90,7 +90,7 @@ class SWP_Meta_Box_Loader {
             'name'  => '<i class="sw swp_twitter_icon"></i> ' . __( 'Custom Tweet','social-warfare' ),
             'desc'  => ( $twitter_id ? sprintf( __( 'If this is left blank your post title will be used. Based on your username (@%1$s), <span class="tweetLinkSection">a link being added,</span> and the current content above, your tweet has %2$s characters remaining.','social-warfare' ),str_replace( '@','',$twitter_handle ),'<span class="counterNumber">140</span>' ) : sprintf( __( 'If this is left blank your post title will be used. <span ="tweetLinkSection">Based on a link being added, and</span> the current content above, your tweet has %s characters remaining.','social-warfare' ),'<span class="counterNumber">140</span>' )),
             'id'    => $prefix . 'custom_tweet',
-            'class' => $prefix . 'customTweetWrapper',
+            'class' => $prefix . 'custom_tweetWrapper',
             'type'  => 'textarea',
             'clone' => false,
         );
