3d160c5412f50c5cf645365e6e6759f36e5814cd
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-09-05 16:11:15 -0400

    Replaced h4 with p.

--- lib/meta-box/inc/fields/display-text.php
+++ lib/meta-box/inc/fields/display-text.php
@@ -25,7 +25,7 @@ class SWPMB_Display_Text_Field extends SWPMB_Field
 	static function begin_html( $meta, $field )
 	{
 		$attributes = empty( $field['id'] ) ? '' : " id='{$field['id']}'";
-		return sprintf( '<h4%s>%s</h4>', $attributes, $field['name'] );
+		return sprintf( '<p%s>%s</p>', $attributes, $field['name'] );
 	}
 
 	/**
