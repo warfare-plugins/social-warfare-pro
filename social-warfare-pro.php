e03e072d07111e3e37d3b5cbc7c31d7689c1b692
Cortland Mahoney <cortland.mahoney@gmail.com>
2018-09-13 12:34:45 -0400

    Removed deprecated mishmatch_notification; updated 'swp_needs_core()' to use old mismatch_notification text.

--- social-warfare-pro.php
+++ social-warfare-pro.php
@@ -61,27 +61,10 @@ function initialize_social_warfare_pro() {
 
 if ( !function_exists( 'swp_needs_core' ) ) :
     function swp_needs_core() {
-        ?>
-        <div class="update-nag notice is-dismissable">
-            <p><b>Important:</b> You currently have Social Warfare - Pro installed without our Core plugin installed.<br/>Please download the free core version of our plugin from the WordPress repo or from our <a href="https://warfareplugins.com" target="_blank">website</a>.</p>
-        </div>
-        <?php
+        echo '<div class="update-nag notice is-dismissable"><p><b>Important:</b> You are currently running Social Warfare v' . SWP_VERSION . ' and Social Warfare - Pro v' . SWPP_VERSION . '. In order to avoid conflicts, these two version need to match in order to activate all of the plugins features. Please update the appropriate plugin so that both Social Warfare and Social Warfare - Pro are on the same version. For more information about this, <a href="https://warfareplugins.com/support/updating-social-warfare-social-warfare-pro/">please read this</a></p></div>';
     }
 endif;
 
-
-/**
- * Notify users that the versions of Social Warfare and Social Warfare Pro are mismatched.
- *
- *
- * @since  2.2.0
- * @param  none
- * @return void
- *
- */
- function mismatch_notification() {
-    echo '<div class="update-nag notice is-dismissable"><p><b>Important:</b> You are currently running Social Warfare v' . SWP_VERSION . ' and Social Warfare - Pro v' . SWPP_VERSION . '. In order to avoid conflicts, these two version need to match in order to activate all of the plugins features. Please update the appropriate plugin so that both Social Warfare and Social Warfare - Pro are on the same version. For more information about this, <a href="https://warfareplugins.com/support/updating-social-warfare-social-warfare-pro/">please read this</a></p></div>';
-}
 
 /**
  * Notify users that the versions of Social Warfare and Social Warfare Pro are mismatched.
