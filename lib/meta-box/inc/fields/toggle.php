d2b1c83dc891262fe8634760a3841669e15bb0ea
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-02-14 13:42:51 -0500

    Changed source of post_id; spaces to tabs.

--- lib/meta-box/inc/fields/toggle.php
+++ lib/meta-box/inc/fields/toggle.php
@@ -42,40 +42,41 @@ class SWPMB_Toggle_Field extends SWPMB_Field
 		}
 
 		$value = SWPMB_Toggle_Field::swp_get_value( $field['id'] );
-        $status = $value === 'true' ? 'on' : 'off';
+		$status = $value === 'true' ? 'on' : 'off';
 
 		$label = '<div class="swpmb-label">';
-		    $label .= '<label for="' . $field['id'] . '">' . $field['name'] . '</label>';
+			$label .= '<label for="' . $field['id'] . '">' . $field['name'] . '</label>';
 		$label .='</div>';
 
-        $toggle = "<div class='sw-checkbox-toggle swp-post-editor' status='$status' field='#${field['id']}'>
-                       <div class='sw-checkbox-on'>ON</div>
-                       <div class='sw-checkbox-off'>OFF</div>
-                   </div>";
+		$toggle = "<div class='sw-checkbox-toggle swp-post-editor' status='$status' field='#${field['id']}'>
+					   <div class='sw-checkbox-on'>ON</div>
+					   <div class='sw-checkbox-off'>OFF</div>
+				   </div>";
 
-        //* @TODO
+		//* @TODO
 		//* The type is set to 'text'. It should be 'checkbox'. This is a patch for 3.4.1 only with the corresponding JS in
 		//* admin.js setTempConditionalField
-        $toggle .= "<input id='{$field['id']}' name='{$field['id']}' value='$value' type='text' style='display: none;'  />";
+		$toggle .= "<input id='{$field['id']}' name='{$field['id']}' value='$value' type='text' style='display: none;'  />";
 
-        //* Close the div opened in begin_html().
-        return $label . $toggle . '</div>';
+		//* Close the div opened in begin_html().
+		return $label . $toggle . '</div>';
 	}
 
-    /**
-     * Fetches the stored value for a toggle, if it exists.
+	/**
+	 * Fetches the stored value for a toggle, if it exists.
 	 *
 	 * If it DNE, then use a default provided by a map of defaults.
 	 *
-     * @param  string $key The post meta key to try fetching.
-     * @return bool        The value for this key.
-     * @since  3.4.0 | 24 OCT 2018 | Created
-     *
-     */
+	 * @param  string $key The post meta key to try fetching.
+	 * @return bool        The value for this key.
+	 * @since  3.4.0 | 24 OCT 2018 | Created
+	 *
+	 */
 	static function swp_get_value( $key ) {
-		$post_id = ( int ) $_GET['post'];
+		global $post;
+		$post_id = $post->ID;
 		$value = get_post_meta( $post_id, $key, true );
-        $value = is_array( $value ) ? $value[0] : $value;
+		$value = is_array( $value ) ? $value[0] : $value;
 
 		$defaults = array(
 			'swp_twitter_use_open_graph'	=> 'true',
