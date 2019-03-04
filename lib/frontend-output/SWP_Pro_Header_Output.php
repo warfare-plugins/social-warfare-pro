f02d7a546f332264154854462e49366cb48029bf
Cortland Mahoney <cortland.mahoney@gmail.com>
2019-03-04 17:01:48 -0500

    Updated variable names; added comments.

--- lib/frontend-output/SWP_Pro_Header_Output.php
+++ lib/frontend-output/SWP_Pro_Header_Output.php
@@ -134,6 +134,7 @@ class SWP_Pro_Header_Output extends SWP_Header_Output {
 			$fields[$og_key] = $value;
 		}
 
+		// echo __METHOD__, die(var_dump($fields));
 		$fields = array_map( 'htmlspecialchars', $fields );
 		$this->open_graph_data = array_merge( $known_fields, $fields );
 	}
@@ -150,18 +151,23 @@ class SWP_Pro_Header_Output extends SWP_Header_Output {
 	 *
 	 */
 	protected function fetch_social_warfare_open_graph_fields() {
+		// echo __METHOD__;
 		$fields = array(
-			'og_title',         // These have a meta field.
+			// These have a meta field.
+			'og_title',
 			'og_description',
 			'og_image_url',
 			'og_image_width',
 			'og_image_height',
-			'og_url',           // These do not have a meta field.
+			// These do not have a meta field.
+			'og_url',
 			'og_site_name',
 		);
 
 		foreach ($fields as $index => $key) {
 			$maybe_value = SWP_Utility::get_meta( $this->post->ID, "swp_$key" );
+			// echo "<br>key: swp_$key";
+			// echo "<br>$maybe_value";
 			// Go from indexed array to associative, with possibly missing values.
 			unset($fields[$index]);
 			$fields[$key] = $maybe_value;
@@ -358,13 +364,13 @@ class SWP_Pro_Header_Output extends SWP_Header_Output {
 			'_yoast_wpseo_twitter-image' => 'twitter_image'
 		);
 
-		foreach( $yoast_to_twitter as $yoast => $twitter ) {
-			$maybe_value = SWP_Utility::get_meta( $this->post->ID, $yoast );
-			if ( !empty( $maybe_value ) ) {
+		foreach( $yoast_to_twitter as $yoast_key => $twitter_key ) {
+			$value = SWP_Utility::get_meta( $this->post->ID, $yoast_key );
+			if ( !empty( $value ) ) {
 				if ( function_exists (' wpseo_replace_vars' ) ) {
-					$maybe_value = wpseo_replace_vars( $maybe_value, $this->post );
+					$maybe_value = wpseo_replace_vars( $value, $this->post );
 				}
-				$fields[$twitter] = $maybe_value;
+				$fields[$twitter_key] = $value;
 			}
 		}
 
@@ -393,10 +399,12 @@ class SWP_Pro_Header_Output extends SWP_Header_Output {
 			}
 		}
 
-		if ( false !== SWP_Utility::get_meta( $this->post->ID, 'swp_twitter_use_open_graph' ) ) {
+		// Apply the OG data as a priority over twitter data
+		if ( true == SWP_Utility::get_meta( $this->post->ID, 'swp_twitter_use_open_graph' ) ) {
 			return array_merge($twitter_fields, $shared_fields);
 		}
 
+		// Apply Twitter data the prioritized data.
 		return array_merge( $shared_fields, $twitter_fields );
 	}
 
