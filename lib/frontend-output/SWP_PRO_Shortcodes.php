9ab256a3be99332d940f695e93f421cc1382212a
Nicholas Z. Cardot <nick@sitesketch101.com>
2019-07-09 15:20:04 -0500

    Creation of the SWP_Pro_Shortcodes class.

---
+++ lib/frontend-output/SWP_PRO_Shortcodes.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * A class to house and register all of the Social Warfare - Pro advanced
+ * shortcodes for use in posts and widgets (if a widget shortcode plugin is
+ * active).
+ *
+ * @package   SocialWarfare\Frontend-Output
+ * @copyright Copyright (c) 2018, Warfare Plugins, LLC
+ * @license   GPL-3.0+
+ * @since     4.0.0 | 09 JUL 2019 | Created
+ *
+ */
+class SWP_Pro_Shortcodes {
+
+}
