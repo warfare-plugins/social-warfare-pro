6840b2e9ad0c9045150880941110926709c436aa
Nicholas Cardot <nick@sitesketch101.com>
2020-08-20 16:58:16 -0500

    Built out the get_input_score() method.

--- lib/analytics/SWP_Pro_Social_Optimizer.php
+++ lib/analytics/SWP_Pro_Social_Optimizer.php
@@ -154,10 +154,10 @@ class SWP_Pro_Social_Optimizer {
 		foreach( $this->fields as $field ) {
 
 			// Get the score for this individual field.
-			$this->scores[$field] = $this->get_individual_score( $field );
+			$this->scores[$field] = $this->get_field_score( $field );
 
 			// Update the total score.
-			$this->scores['total'] =+ $this->scores[$field]['current_score'];
+			$this->scores['total'] += $this->scores[$field]['current_score'];
 		}
 
 		// Round the total score to the nearest whole number.
@@ -170,54 +170,6 @@ class SWP_Pro_Social_Optimizer {
 
 
 	/**
-	 * The establish_maximum_scores() method will provide the baseline for how
-	 * many points each field can be worth. These scores need to always add up
-	 * to 100 points. As such, if the Twitter card fields are activated we will
-	 * have 9 fields adding up to 100 versus 6 fields without them. So this
-	 * allows us to check that field and assign those maximum values accordingly.
-	 *
-	 * @since  4.2.0 | 20 AUG 2020 | Created
-	 * @param  void
-	 * @return void
-	 *
-	 */
-	private function establish_maximum_scores() {
-
-		// If the Twitter fields don't exist, we have 6 fields.
-		if( true == $this->get_field('swp_twitter_use_open_graph') ) {
-			$max_grades = array(
-				'swp_og_image' => 20,
-				'swp_og_title' => 15,
-				'swp_og_description' => 15,
-				'swp_custom_tweet' => 15,
-				'swp_pinterest_image' => 20,
-				'swp_pinterest_description' => 15
-			);
-
-		// If they do exist, we have all 9 fields to grade.
-		} else {
-			$max_grades = array(
-				'swp_og_image' => 15,
-				'swp_og_title' => 10,
-				'swp_og_description' => 10,
-				'swp_custom_tweet' => 10,
-				'swp_twitter_card_image' => 10,
-				'swp_twitter_card_title' => 10,
-				'swp_twitter_card_description' => 10,
-				'swp_pinterest_image' => 15,
-				'swp_pinterest_description' => 10
-			);
-		}
-
-		// Loop through and add each one to our existing $field_data property.
-		foreach( $max_grades as $key => $value ) {
-			$this->field_data[$key]['max_grade'] = $value;
-			$this->fields[] = $key;
-		}
-	}
-
-
-	/**
 	 * The get_individual_score() method will calculate and return the score for
 	 * any one of the given fields.
 	 *
@@ -226,7 +178,7 @@ class SWP_Pro_Social_Optimizer {
 	 * @return array  An array of score data.
 	 *
 	 */
-	private function get_individual_score( $field ) {
+	private function get_field_score( $field ) {
 
 		switch( $this->field_data[$field]['type'] ) {
 
@@ -237,7 +189,7 @@ class SWP_Pro_Social_Optimizer {
 
 			// If the field is an input field.
 			case 'input':
-				// $scores = $this->get_input_score();
+				$scores = $this->get_input_score( $field );
 				break;
 		}
 		return $scores;
@@ -328,17 +280,145 @@ class SWP_Pro_Social_Optimizer {
 
 
 		// The number of factors in this score.
-		$factors     = 2;
+		$factors = 2;
 
 		// Convert the percentages into points.
 		$ratio_score = $this->calculate_subscores( $ratio_percent, $factors, $scores['max_score']);
 		$size_score  = $this->calculate_subscores( $size_percent, $factors, $scores['max_score']);
 
+		// Populate the scores data with our updated numbers.
 		$scores['current_score'] = round( $ratio_score + $size_score );
-		$scores['percent'] = round( $scores['current_score'] / $scores['max_score'] );
+		$scores['percent']       = round( $scores['current_score'] / $scores['max_score'] );
 
+		// Return the scores data.
 		return $scores;
+	}
 
+
+	/**
+	 * The get_input_score() will tabulate the score for any fields that are of
+	 * the type "input". Most inputs fields will be sent to this method, however,
+	 * some fields have specialized attributes, and as such, they will use their
+	 * own method for calculations.
+	 *
+	 * @since  4.2.0 | 20 AUG 2020 | Created
+	 * @param  string $field The name of the field
+	 * @return array  The array of score data
+	 *
+	 */
+	private function get_input_score( $field ) {
+
+		// Establish our default $scores array.
+		$scores = array(
+			'percent' => 0,
+			'current_score' => 0,
+			'max_score' => $this->field_data[$field]['max_grade']
+		);
+
+		// Fetch and organize the data used in the calculations.
+		$input_text   = $this->get_field( $field );
+		$input_length = strlen( $input_text );
+		$max_length   = $this->field_data[$field]['max_length'];
+		$ideal_length = $this->field_data[$field]['length'];
+
+
+		/**
+		 * This section will check the length of the field and compare it to the
+		 * ideal. If it is below 70 characters, we'll divide to length by that
+		 * number to come up with a percentage. For example, the user might be
+		 * 95% of the way there. If they are over 100, we'll divide the input
+		 * length into 100. So for example, if they have 105, they are 5% over.
+		 *
+		 * The resulting variable, length_percent, will be used below as one of
+		 * the factors to come up with our total score for this field.
+		 *
+		 */
+		if( 0 < $input_length && $input_length < $ideal_length ) {
+			$length_percent = $input_length / $ideal_length;
+		} elseif( 0 === $input_length ) {
+			$length_percent = 0;
+		} else {
+			$length_percent = $ideal_length / ($ideal_length + (($input_length - $ideal_length) * 2));
+		}
+
+
+		/**
+		 * This portion of the code will check to ensure that the user has filled
+		 * out the input and that the input does not exceed the maximum number
+		 * of characters for this field.
+		 *
+		 * The resulting variable, max_length_percent, will be used below as one
+		 * of the factors to come up with our total score for this field.
+		 *
+		 */
+		if( 0 < $input_length && $input_length < $max_length ) {
+			$max_length_percent = 1;
+		} else {
+			$max_length_percent = 0;
+		}
+
+
+		// The number of factors in this score.
+		$factors = 2;
+
+		// Convert the percentages into points.
+		$length_score     = $this->calculate_subscores( $length_percent, $factors, $scores['max_score']);
+		$max_length_score = $this->calculate_subscores( $max_length_percent, $factors, $scores['max_score']);
+
+		// Populate the scores data with our updated numbers.
+		$scores['current_score'] = round( $length_score + $max_length_score );
+		$scores['percent']       = round( $scores['current_score'] / $scores['max_score'] );
+
+		// Return the scores data.
+		return $scores;
+	}
+
+
+	/**
+	 * The establish_maximum_scores() method will provide the baseline for how
+	 * many points each field can be worth. These scores need to always add up
+	 * to 100 points. As such, if the Twitter card fields are activated we will
+	 * have 9 fields adding up to 100 versus 6 fields without them. So this
+	 * allows us to check that field and assign those maximum values accordingly.
+	 *
+	 * @since  4.2.0 | 20 AUG 2020 | Created
+	 * @param  void
+	 * @return void
+	 *
+	 */
+	private function establish_maximum_scores() {
+
+		// If the Twitter fields don't exist, we have 6 fields.
+		if( true == $this->get_field('swp_twitter_use_open_graph') ) {
+			$max_grades = array(
+				'swp_og_image' => 20,
+				'swp_og_title' => 15,
+				'swp_og_description' => 15,
+				'swp_custom_tweet' => 15,
+				'swp_pinterest_image' => 20,
+				'swp_pinterest_description' => 15
+			);
+
+		// If they do exist, we have all 9 fields to grade.
+		} else {
+			$max_grades = array(
+				'swp_og_image' => 15,
+				'swp_og_title' => 10,
+				'swp_og_description' => 10,
+				'swp_custom_tweet' => 10,
+				'swp_twitter_card_image' => 10,
+				'swp_twitter_card_title' => 10,
+				'swp_twitter_card_description' => 10,
+				'swp_pinterest_image' => 15,
+				'swp_pinterest_description' => 10
+			);
+		}
+
+		// Loop through and add each one to our existing $field_data property.
+		foreach( $max_grades as $key => $value ) {
+			$this->field_data[$key]['max_grade'] = $value;
+			$this->fields[] = $key;
+		}
 	}
 
 	private function calculate_subscores( $percent, $factors, $max_score ) {
