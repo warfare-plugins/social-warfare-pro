4ad479dcb2868a2ba51bd9ec0555bf08f616e448
Nicholas Cardot <nick@sitesketch101.com>
2020-08-20 06:50:08 -0500

    Created the Social Optimizer server-side class.

---
+++ lib/analytics/SWP_Pro_Social_Optimizer.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The Social Optimizer class will do the same thing as the Gutenberg sidebar,
+ * except this one will run the scoring on the server side allowing us to go
+ * back through old posts and generate optimization scores for them. This will
+ * also make it easier for us to store the grades in the database. We will then
+ * use these numbers to make recommendations for the user to go back and optimize
+ * their highest performing posts with the least amount of optimizations.
+ *
+ * @since 4.2.0 | 20 AUG 2020 | Created
+ *
+ */
+class SWP_Pro_Social_Optimizer {
+	
+}
